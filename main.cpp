d72cba9428f1dcc9c197c6b3933d81d0c3345750
janblocker2003 <janblocker2003@gmail.com>
2026-02-07 03:11:39 +0000

    head hurts, progress but some errors especially with referencing private attributes, giving up, good enough

--- main.cpp
+++ main.cpp
@@ -5,23 +5,22 @@
 void testHorse();
 
 int main(){
-	std::cout << "OOP Horse Race!!" << std::endl;
-	
-	testHorse();
-	
-	return 0;
-}
+	for (int i = 0; i < Race::NUM_HORSES; i++){
+		i.init(1, 15);
+		bool keepGoing = true;
+		Horse temp;
+		temp.init(i, 0);
+		while (keepGoing){
+			temp.advance();
+			temp.printLane();
+        		std::cout << "Press enter for another turn" << std::endl;
+                		std::cin.ignore();
+			if (temp.isWinner()){
+				keepGoing = false;
+				std::cout << "Horse " << temp.index << " is the winner!! " << std::endl;
 
-void testHorse(){
-	Horse h;
-	h.init(1, 15);
-	bool keepGoing = true;
-	while (keepGoing){
-		h.advance();
-		h.printLane();
-		if (h.isWinner()){
-			keepGoing = false;
-		} //end if
-	} //end while
-} //end test
-
+			} //end if
+		} //end while
+	} //end for
+return 0;
+} //end main
